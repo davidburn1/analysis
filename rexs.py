2163141b9cae2c566ac548c0b602e59c59a268b7
David Burn <david.burn@diamond.ac.uk>
2020-09-24 08:05:39 +0100

    update rexs phase diagrams

--- rexs.py
+++ rexs.py
@@ -2,9 +2,9 @@ import numpy as np
 import h5py
 from PIL import Image
 
-from ascii_data import loadI10Data
+#from ascii_data import loadI10Data
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def qgrid(E, cth, center_x=0, center_y=0):
     """
@@ -61,34 +61,36 @@ def phaseDiagram_tiff2hdf_fieldScan(directory, temperatures, fields, scanIDs, bi
     """
     convert a phase diagram script into a single hdf file
     Assume the cth and ctth angles stays constant
+    Field scans at constant temperatures
     """
 
     grid = np.zeros((len(temperatures), len(fields), 2048//bins, 2048//bins))
 
 
-    for i,s in enumerate(scanIDs):
-	    print i, s
-	    #f = h5py.File(directory + 'i10-%06d.nxs' % s,'r')
-	    #cth = f['entry1']['before_scan']['cth']['cth'][()]
-	    #energy = f['entry1']['before_scan']['pgm_energy']['pgm_energy'][()]
-	    #image_fns = f['entry1']['instrument']['pimtetiff']['data_file']['file_name']#[()]
-	    #f.close()
-	    data = loadI10Data(directory + 'i10-%06d.dat' % s)
-	    cth = 90 - float(data['metadata']['th'] )
-	    energy = float(data['metadata']['pgm_energy'])
-	    image_fns = [directory + "%06d-pimte-files/%05d.tif" % (s, int(x)) for x in data['path']]
+    for i in range(len(scanIDs)):
+        print(i, scanIDs[i], temperatures[i])
+        f = h5py.File(directory + 'i10-%06d.nxs' % scanIDs[i],'r')  
+        cth = f['entry1']['before_scan']['cth']['cth'][()]
+        energy = f['entry1']['before_scan']['pgm_energy']['pgm_energy'][()]
+        image_fns = f['entry1']['instrument']['pimtetiff']['data_file']['file_name'][()]
+        #print image_fns
+        f.close()
+	    #data = loadI10Data(directory + 'i10-%06d.dat' % s)
+	    #cth = 90 - float(data['metadata']['th'] )
+	    #energy = float(data['metadata']['pgm_energy'])
+	    #image_fns = [directory + "%06d-pimte-files/%05d.tif" % (s, int(x)) for x in data['path']]
 
 	    
-	    QX,QY = qgrid(energy, cth, center_x=0, center_y=0)
-	    QX = QX.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
-	    QY = QY.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
+        QX,QY = qgrid(energy, cth, center_x=0, center_y=0)
+        QX = QX.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
+        QY = QY.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
 
 
-	    for j,fn in enumerate(image_fns):
-		    im = np.array(Image.open(fn))
-		    im = im.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
-		    grid[i,j,:,:] = im
-		    del im
+        for j,fn in enumerate(image_fns):
+            im = np.array(Image.open(fn))
+            im = im.reshape(2048//bins, bins, 2048//bins, bins).mean(1).mean(2)
+            grid[i,j,:,:] = im
+            del im
 
 
             #plt.pcolormesh(QX,QY, grid[i,j,:,:], cmap='gist_ncar_r', vmin=250, vmax=3000)
@@ -113,19 +115,19 @@ def phaseDiagram_tiff2hdf_temperatureScan(directory, temperatures, fields, scanI
     grid = np.zeros((len(temperatures), len(fields), 2048//bins, 2048//bins))
 
 
-    for i,s in enumerate(scanIDs):
-        print i, s
-        #f = h5py.File(directory + 'i10-%06d.nxs' % s,'r')
-        #cth = f['entry1']['before_scan']['cth']['cth'][()]
-        #energy = f['entry1']['before_scan']['pgm_energy']['pgm_energy'][()]
-        #image_fns = f['entry1']['instrument']['pimtetiff']['data_file']['file_name'][()]
-        #temperatures_raw = f['entry1']['instrument']['temperature']['sample'][()]
-	#f.close()
-	data = loadI10Data(directory + 'i10-%06d.dat' % s)
-	cth = 90 - float(data['metadata']['th'] )
-	energy = float(data['metadata']['pgm_energy'])
-	image_fns = [directory + "%06d-pimte-files/%05d.tif" % (s, int(x)) for x in data['path']]
-        temperatures_raw = data['Channel1Temp']
+    for i in range(len(scanIDs)):
+        print(i, scanIDs[i], fields[i])
+        f = h5py.File(directory + 'i10-%06d.nxs' % scanIDs[i], 'r')
+        cth = f['entry1']['before_scan']['cth']['cth'][()]
+        energy = f['entry1']['before_scan']['pgm_energy']['pgm_energy'][()]
+        image_fns = f['entry1']['instrument']['pimtetiff']['data_file']['file_name'][()]
+        temperatures_raw = f['entry1']['instrument']['temperature']['sample'][()]
+        f.close()
+        #data = loadI10Data(directory + 'i10-%06d.dat' % s)
+        #cth = 90 - float(data['metadata']['th'] )
+        #energy = float(data['metadata']['pgm_energy'])
+        #image_fns = [directory + "%06d-pimte-files/%05d.tif" % (s, int(x)) for x in data['path']]
+        #temperatures_raw = data['Channel1Temp']
 
 	    
         QX,QY = qgrid(energy, cth, center_x=0, center_y=0)
@@ -137,6 +139,7 @@ def phaseDiagram_tiff2hdf_temperatureScan(directory, temperatures, fields, scanI
         temperatures_raw = temperatures_raw[argsort]
         image_fns = np.array(image_fns)[argsort]
 
+        # find the id of the image which has the closest temperature to the regular grid temperatures
         idmap =  np.interp(temperatures, temperatures_raw, np.arange(len(temperatures_raw)))
         idmap = np.round(idmap).astype(int)
 
